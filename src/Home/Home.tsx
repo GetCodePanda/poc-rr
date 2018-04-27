68ff46d5166d601d8246a453b1ea2c2ab0d03093
codepanda <hicodepanda@gmail.com>
2018-04-27 17:36:32 +0530

    impllementing dump mapDispatchToProps in home component

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,19 +1,56 @@
 import * as React from 'react';
 import Layout from '../common/Layout/Layout';
 import {connect} from 'react-redux';
+import { Dispatch } from 'redux';
 
-class Home extends React.Component {
+
+interface IHomeProps {
+    home : any,
+    changeContentOne: () => Dispatch,
+    changeContentTwo: () => Dispatch
+}
+
+class Home extends React.Component<IHomeProps,{}> {
+
+    public handleClick = (contentIndex : string) => {
+        (contentIndex === 'one') 
+            ? this.props.changeContentOne()
+            : this.props.changeContentTwo()
+    }
+    
     public render(){
         return (
             <Layout>
-                <h3> Home Page </h3>
+                <h3> { this.props.home.title } </h3>
+                <p>{this.props.home.content}</p>
+                <button onClick={this.handleClick.bind(this,'one')}>change content to 1</button>
+                <br/>
+                <br/>
+                <button onClick={this.handleClick.bind(this,'two')}>change content to 2</button>
             </Layout>
         )
     }
 }
 
-export default connect((state:any, props :any) => {
+const mapStateToProps = (state:any, props :any) => {
     return {
         home: state.home
     }
- })(Home);
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        changeContentOne : () => {
+            dispatch({
+                type: "CHANGE_CONTENT_TO_CONTENT_ONE"
+            })
+        },
+        changeContentTwo: () => {
+            dispatch({
+                type: "CHANGE_CONTENT_TO_CONTENT_TWO"
+            })
+        }
+    }
+}
+
+export default connect<{}, {}, IHomeProps>(mapStateToProps , mapDispatchToProps)(Home);
